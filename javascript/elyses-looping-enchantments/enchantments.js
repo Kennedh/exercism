3f7b88d71ab4d6303f32248e194270e0a6f96167
kennedh <ken.cmarcellino@gmail.com>
2026-08-02 15:52:34 -0300

    feat: resolve desafio enchantments em JS

---
+++ javascript/elyses-looping-enchantments/enchantments.js
@@ -0,0 +1,39 @@
+// @ts-check
+
+/**
+ * Determine how many cards of a certain type there are in the deck
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {number} number of cards of a single type there are in the deck
+ */
+export function cardTypeCheck(stack, card) {
+  let cards = 0;
+  stack.forEach(item => {
+    if (item == card){
+      cards += 1;
+    }
+  });
+  return cards;
+}
+
+/**
+ * Determine how many cards are odd or even
+ *
+ * @param {number[]} stack
+ * @param {boolean} type the type of value to check for - odd or even
+ * @returns {number} number of cards that are either odd or even (depending on `type`)
+ */
+export function determineOddEvenCards(stack, type) {
+  let res = 0;
+  for (const card of stack) {
+    if (type && card % 2 == 0){
+      res += 1;
+    }
+    else if (!type && card % 2 !== 0){
+      res += 1;
+    };
+  }
+  return res;
+}
